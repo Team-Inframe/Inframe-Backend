e5c95de160da297ad68c8147d1e13e3af2bf8d24
kimzini <rlawlsdl11111@gmail.com>
2025-01-16 02:37:21 +0900

    feat: ai 배경 생성 기능 구현

--- frame/urls.py
+++ frame/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateFrameView
+from .views import CreateFrameView, CreateAiFrameView
 
 urlpatterns = [
     path('', CreateFrameView.as_view(), name='create-frame'),
+    path('images/', CreateAiFrameView.as_view(), name='create-frame-images'),
 ]
 
