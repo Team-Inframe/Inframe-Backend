91417e82b0c4849f2ec023b8fb7b0815f41f5b5b
kimzini <rlawlsdl11111@gmail.com>
2025-01-15 23:52:54 +0900

    feat: 스티커 생성 기능 구현

--- sticker/models.py
+++ sticker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Sticker(models.Model):
+    id = models.AutoField(primary_key=True)
+    # userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    stickerUrl = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    isDeleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "sticker"
