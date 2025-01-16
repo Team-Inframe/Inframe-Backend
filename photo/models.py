d1c36abd5b314083135299bca14671e32d24cffc
kimzini <rlawlsdl11111@gmail.com>
2025-01-17 00:45:21 +0900

    feat: 갤러리 목록 조회 기능 구현

--- photo/models.py
+++ photo/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from user.models import User
 
 class Photo(models.Model):
-    photoId = models.AutoField(primary_key=True)
-    userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='userId')
-    photoUrl = models.CharField(max_length=1000)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    isDeleted = models.BooleanField(default=False)
+    photo_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo_url = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
     class Meta:
         db_table = 'photo'
 
