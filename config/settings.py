7f44a50e1b826b93febd5e3b1de11c919495a4cc
j0hun <dudgns3681@gmail.com>
2025-01-18 14:04:57 +0900

    feat: cors 관련 코드 추가

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,11 @@ INSTALLED_APPS = [
     'user',
     'custom_frame',
     'photo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
