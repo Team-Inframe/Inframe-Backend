6209b61c8915b4338111191bbb050be1f191b135
kimzini <rlawlsdl11111@gmail.com>
2025-01-20 22:18:47 +0900

    feat: 강제 https 적용

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+SECURE_SSL_REDIRECT = env('DJANGO_SECURE_SSL_REDIRECT')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
