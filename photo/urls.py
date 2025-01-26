7376e79c5db1a59e560b06ba16f3fb32ab5510ab
hyeonho <tylee12@naver.com>
2025-01-26 16:06:50 +0900

    feat: 사진 단일 조회 기능 추가

--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,6 @@ from photo.views import CreatePhotoView, PhotoListView, PhotoSingleView
 urlpatterns = [
     path("", CreatePhotoView.as_view(), name="photo-create"),
     path("lists", PhotoListView.as_view(), name="photo-list"),
-    path("singleview", PhotoSingleView.as_view(), name="photo-single-view"),
+    path("<int:photo_id>", PhotoSingleView.as_view(), name="photo-single-view"),
 
 ]
