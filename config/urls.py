a18b60b09d923289bdc76f9d6d078ed7b2c1b8cd
hyeonho <tylee12@naver.com>
2025-01-30 15:31:19 +0900

    feat: 스티커 프롬프트 수정

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    url=f"{settings.SERVER_URL}/api/v1"
+    #url=f"{settings.SERVER_URL}/api/v1"
 )
 
 urlpatterns = [
